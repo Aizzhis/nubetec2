32cb6ed1f3d4d13769063f2209c8c5af4cd38b00
Aizzhis <138122799+Aizzhis@users.noreply.github.com>
2023-10-10 16:43:24 -0300

    Add files via upload

---
+++ funcao.py
@@ -0,0 +1,15 @@
+def somar(x, y):
+    return x + y
+
+def subtrair(x, y):
+    return x - y
+
+def dividir(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Erro: Divisão por zero"
+
+def multiplicar(x, y):
+    return x * y
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import funcao
+
+print(funcao.somar(10, 15))
+print(funcao.subtrair(33, 15))
+print(funcao.dividir(128, 2))
+print(funcao.multiplicar(3, 3.141592))
